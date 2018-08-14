cbb7144fa757b572602b71b0bc43ce84017ec0e5
Engineer_X | BARYON.NETWORK <journeyrider@live.com>
2018-08-14 19:03:01 -0400

    Create blockcheck.sh

---
+++ blockcheck.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#########################
+### Block Check Utility for Cosmos Network blockchain fullnodes and validators
+### takes two parameters block and ValidatorAddress
+### defaults to ValidatorAddress 3AAD96F8B01F73344079C1D030B7AF5351BF9C11
+### example Block Check Utility script call
+### ./blockcheck.sh bc 106733 3AAD96F8B01F73344079C1D030B7AF5351BF9C11
+###
+### convient alias installation:
+### echo 'alias bc="~/blockcheck.sh"'>>~/.bash_profile
+### source ~/.bash_profile
+### call in a loop from your terminal example:
+### while true; do bc 106733 3AAD96F8B01F73344079C1D030B7AF5351BF9C11; sleep 2; done
+#########################
+
+
+block=${1:-DEFAULTVALUE}
+vaddr=${2:-3AAD96F8B01F73344079C1D030B7AF5351BF9C11}
+
+urlstr='http://localhost:26657/commit?'$block
+
+echo -n $(date +"%Y%m%d%H%M%S") ' '
+check=$(curl -s $urlstr|grep $vaddr)
+if [ -z "$check" ]
+  then
+    echo "MISSED BLOCK $block"
+  else
+    echo 'block: '$block' '$check
+fi
+
